48cbd5713908d08821bf57ee4b33021e5b891e22
Klir Balista <klirbalista@gmail.com>
2025-11-10 03:22:48 +0800

    Update navigation for renamed billing to item-charges

--- src/app/(staff)/[birthcare_Id]/Navigation.jsx
+++ src/app/(staff)/[birthcare_Id]/Navigation.jsx
@@ -214,9 +214,9 @@ const Navigation = ({ user }) => {
       hasSubItems: true,
       subItems: [
         {
-          href: `/${birthcare_Id}/billing`,
+          href: `/${birthcare_Id}/item-charges`,
           label: "Item Charges",
-          permission: "manage_billing",
+          permission: "manage_item_charges",
           icon: "M20 6h-2.18c.11-.31.18-.65.18-1a2.996 2.996 0 00-5.5-1.65l-.5.67-.5-.68C10.96 2.54 10.05 2 9 2 7.34 2 6 3.34 6 5c0 .35.07.69.18 1H4c-1.11 0-2 .89-2 2v11c0 1.11.89 2 2 2h16c1.11 0 2-.89 2-2V8c0-1.11-.89-2-2-2z",
         },
         {
