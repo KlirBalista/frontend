e265b3bf233999182a7021dddf99038cf6daf5d3
Klir Balista <klirbalista@gmail.com>
2025-11-14 19:15:41 +0800

    button dialog

--- src/app/(staff)/[birthcare_Id]/admission-list/PatientAdmissionModal.jsx
+++ src/app/(staff)/[birthcare_Id]/admission-list/PatientAdmissionModal.jsx
@@ -350,7 +350,7 @@ const PatientAdmissionModal = ({ isOpen, onClose, birthcare_Id, onAdmissionCreat
         confirmText="OK"
       />
 
-      <div className="fixed inset-0 z-50 overflow-y-auto">
+      <div className="fixed inset-0 z-40 overflow-y-auto">
         <div className="flex items-center justify-center min-h-screen px-4 pt-4 pb-20">
           {/* Backdrop */}
           <div 
